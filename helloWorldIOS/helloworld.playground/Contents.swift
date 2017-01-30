f527796fc57c2a3c7652497222471da7cdb91c77
Usuário Convidado <Guest@PA06MICRO19.local>
2017-01-30 21:32:06 -0200

    teste

---
+++ helloWorldIOS/helloworld.playground/Contents.swift
@@ -0,0 +1,20 @@
+let sentence : String =  "1"
+print(sentence)
+var number = 2.0
+
+//Tuplas
+
+var student : (String, Int, Double) = ("Bruno Martini", 27, 10.0)
+
+
+
+var grades: [Double] = [10.0, 5.5, 9.0, 2.5, 10.0, 10.0]
+var grades2 = [Double]()
+var grades3: [Double] = []
+
+grades.append(7.5)
+
+let removedItem = grades.removeAtIndex(5)
+
+
+
